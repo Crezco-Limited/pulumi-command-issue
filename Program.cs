6125a1eff906c1e2a4175b56796afacaae26e26c
Phil Broderick <Phil.Broderick@crezco.com>
2023-07-10 10:37:52 +0100

    Create a resource group to test auth

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿using Pulumi;
+﻿using Pulumi.AzureNative.Resources;
 using Pulumi.Command.Local;
+using Deployment = Pulumi.Deployment;
 
 return await Deployment.RunAsync(() =>
 {
    var command = new Command("no-op-command", new()
    {
       Create = "az storage account list"
+   });
+   
+   var rg = new ResourceGroup("my-resource-group", new ResourceGroupArgs
+   {
+      Location = "uksouth"
    });
 });
