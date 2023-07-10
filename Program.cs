371ba18a9b160d034bbd4046bf4267bd61b542a0
Phil Broderick <Phil.Broderick@crezco.com>
2023-07-10 09:51:37 +0100

    Simple repro

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Pulumi;
+using Pulumi.Command.Local;
+
+return await Deployment.RunAsync(() =>
+{
+   var command = new Command("no-op-command", new()
+   {
+      Create = "az storage account list"
+   });
+});
